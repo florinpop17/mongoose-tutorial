ad53c4b0e33b2e05232677dcd878c5e6883bcf73
Florin Pop <popflorin1705@yahoo.com>
2017-01-24 17:54:10 +0200

    removed default index route & edited users route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/User');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    
+    User.find({}, function(err, docs){
+        res.render('index', {
+            "users" : docs
+        })
+    });
 });
 
 module.exports = router;
