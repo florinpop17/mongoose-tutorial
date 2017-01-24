6f747b8dda577b40a7d38aea7f1afcb0a2f516d3
Florin Pop <popflorin1705@yahoo.com>
2017-01-24 17:50:38 +0200

    create Schema for users

--- models/User.js
+++ models/User.js
@@ -1,3 +1,14 @@
 var db = require('../lib/db');
 
 //Create Schema
+var UserSchema = new db.Schema({
+    email: { type: String, unique: true},
+    username: { type: String, unique: true},
+    password: String
+});
+
+//Assign User Object
+var MyUser = db.mongoose.model('User', UserSchema);
+
+//Exports
+module.exports = MyUser;
